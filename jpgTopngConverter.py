61625ebf786aac31f64f739cf9021155e1bc6e9f
SaiKiranRachuri <94919293+SaiKiranRachuri@users.noreply.github.com>
2021-11-23 21:22:14 +0530

    Update jpgTopngConverter.py

--- jpgTopngConverter.py
+++ jpgTopngConverter.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 count=0
 
-img_folder=sys.argv[1]                            #input("Enter a source folder with \ : ")
+img_folder=sys.argv[1]                            #Alternatively we can take input from cmd using statement as input("Enter a source folder with \ : ")
 out_folder=sys.argv[2]
 
 
@@ -14,7 +14,7 @@ if not os.path.exists(out_folder):
 
 for file_name in os.listdir(img_folder):
 	img=Image.open(f'{img_folder}{file_name}')
-	clean_name=os.path.splitext(file_name)[0]       #this line splits the files by extension and returns a tuple
+	clean_name=os.path.splitext(file_name)[0]       #This line splits the images by imagename and its extension and returns that in a tuple
 	#print(clean_name)
 	img.save(f'{out_folder}{clean_name}.png','png')
 	count+=1
